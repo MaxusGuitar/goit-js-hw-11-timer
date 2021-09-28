44ac8b16cd291f61f094a87945d172a6a6b0037a
MaxusGuitar <maxim1999chikhanov@gmail.com>
2021-09-28 20:47:55 +0300

    3333

--- index.js
+++ index.js
@@ -1,63 +1,37 @@
-const mainTimer = document.querySelector('#timer-1')
-
-function UpdateTimer({days, hours, mins, secs}) {
-    mainTimer.textContent = `${days}:${hours}:${mins}:${secs}`
+function getTime(t) {
+  const time = Date.parse(t) - Date.parse(new Date());
+  const seconds = Math.floor((time / 1000) % 60);
+  const minutes = Math.floor((time / 1000 / 60) % 60);
+  const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  return {
+    'total': time,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
 }
+ 
+function initializeClock(id, t) {
+  const clock = document.getElementById(id);
+  const daysSpan = clock.querySelector('.days');
+  const hoursSpan = clock.querySelector('.hours');
+  const minutesSpan = clock.querySelector('.minutes');
+  const secondsSpan = clock.querySelector('.seconds');
+ 
+  function updateClock() {
+    const time = getTime(t);
+    daysSpan.innerHTML = time.days;
+    hoursSpan.innerHTML = ('0' + time.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + time.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + time.seconds).slice(-2);
 
-class CountdownTimer{
-    constructor({onTick}) {
-        this.intervalID = null
-        this.onTick = onTick
-    }
-
-    start() {
-        const startTime = Date.now()
-
-        this.intervalID = setInterval(() => {
-            const currentTime = Date.now()
-            // console.log(currentTime)
-            const targetDate = currentTime - startTime
-            const t = getTimeNow(targetDate)          
-            this.onTick(t)
-        }, 1000)
-    }
+  }
+ 
+  updateClock();
+  setInterval(updateClock, 1000);
 }
-
-const timer = new CountdownTimer({
-  onTick: UpdateTimer
-});
-    
-
-
-
-function pad(value) {
-    return String(value).padStart(2, '0') // принимает число и ставит 0 спереди, но если число двойное, то ноль не ставит 
-}
-
-
-
-function getTimeNow(time) {
-    /*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)))
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)))
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-    const secs = pad(Math.floor((time % (1000 * 60)) / 1000))
-    return {days, hours, mins, secs}
-}
+ 
+const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); 
+initializeClock('timer-1', deadline);
